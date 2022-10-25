43e759e0d6636ac64c0280538a034182043d9762
Foad Botan <foadbotan@gmail.com>
2022-10-25 18:51:08 +1100

    Refactor App and add text search

--- src/App.js
+++ src/App.js
@@ -1,82 +1,88 @@
-import "./App.css"
-import { useEffect, useState } from "react"
-import Header from "./components/Header"
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
 // Import Materialize
-import M from "materialize-css"
-import Product from "./components/Product"
-import Loading from "./components/Loading"
-import FilterProducts from "./components/FilterProducts"
+import M from "materialize-css";
+import Product from "./components/Product";
+import Loading from "./components/Loading";
+import Category from "./components/Category";
 
 function App() {
   // Initialize material css
-  M.AutoInit()
+  M.AutoInit();
 
-  const [products, setProducts] = useState([])
-  const [loading, setLoading] = useState(true)
-  // Pass  the value of products to filtered products
-  // Better way to reduce fetching data from the server multiple times
-
-  const [filteredProducts, setFilteredProducts] = useState([])
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [search, setSearch] = useState("");
+  const [categories, setCatagories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all products");
 
   const removeItem = (id) => {
-    console.log(id)
-    setFilteredProducts(filteredProducts.filter((product) => product.id != id))
-  }
+    setFilteredProducts(filteredProducts.filter((product) => product.id !== id));
+  };
 
-  const filterMenOnly = () => {
-    console.log("click")
-
-    const menProducts = [...products.filter((product) => product.category === "men's clothing")]
-    setFilteredProducts(menProducts)
-  }
-
-  const filterWomenOnly = () => {
-    const womenProducts = [...products.filter((product) => product.category === "women's clothing")]
-    setFilteredProducts(womenProducts)
-  }
-
-  const filterElectronics = () => {
-    const electronicProducts = [...products.filter((product) => product.category === "electronics")]
-    setFilteredProducts(electronicProducts)
-  }
-
-  const filterJeweleries = () => {
-    const jeweleriesProducts = [...products.filter((product) => product.category === "jewelery")]
-    setFilteredProducts(jeweleriesProducts)
-  }
-
-  const allProducts = () => {
-    return setFilteredProducts(products)
-  }
+  useEffect(() => {
+    setFilteredProducts(
+      products
+        .filter(
+          (product) => product.category === selectedCategory || selectedCategory === "all products"
+        )
+        .filter((product) => product.title.match(RegExp(search, "i")))
+    );
+  }, [selectedCategory, search, products]);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then((res) => res.json())
       .then((data) => {
-        setProducts(data)
-        setLoading(false)
-        setFilteredProducts(data)
+        setProducts(data);
+        setLoading(false);
+        setCatagories(() => ["all products", ...new Set(data.map(({ category }) => category))]);
       })
       .catch((err) => {
-        console.log(err)
-        setLoading(true)
-      })
-  }, [])
+        console.log(err);
+        setLoading(true);
+      });
+  }, []);
 
   return (
     <div>
       <Header />
       <div className="container" style={{ marginTop: "2rem" }}>
-        {/* Component to filter the items on render */}
-        <FilterProducts filterMenOnly={filterMenOnly} filterWomenOnly={filterWomenOnly} allProducts={allProducts} filterElectronics={filterElectronics} filterJeweleries={filterJeweleries} />
+        <div style={{ margin: "2rem 0" }} className="container">
+          {categories.map((category) => (
+            <Category
+              key={category}
+              category={category}
+              isSelected={selectedCategory === category}
+              handleSelect={() => setSelectedCategory(category)}
+            />
+          ))}
+        </div>
 
-        {/*** if Loading  render loading componnent else render
-         * render the products
-         */}
-        <div className="row">{loading ? <Loading /> : filteredProducts.map((product) => <Product product={product} key={product.id} removeItem={removeItem} filterMenOnly={filterMenOnly} filterWomenOnly={filterWomenOnly} />)}</div>
+        <div className="input-field">
+          <input
+            id="search"
+            type="text"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+          <label htmlFor="search">Search</label>
+        </div>
+
+        <div className="row">
+          {loading ? (
+            <Loading />
+          ) : (
+            filteredProducts.map((product) => (
+              <Product product={product} key={product.id} removeItem={removeItem} />
+            ))
+          )}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
